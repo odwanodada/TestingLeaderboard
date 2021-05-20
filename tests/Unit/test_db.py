72883c611456b3c606cc88debecafbdf5cdd574c
odwanodada <odwanodada22@gmail.com>
2021-05-20 16:02:47 +0200

    UnitTesting

--- tests/Unit/test_db.py
+++ tests/Unit/test_db.py
@@ -3,9 +3,10 @@ from website.models import db
 from website.models import  Note , Work
 
 class Test_database(TestCase):
-    def test_signup(self):
+    def test_note_table(self):
         note = Note(id = 1, data = "Odwa", date = 2020/5/21, user_id = 1)
         self.assertEqual(note.id, 1)
         self.assertEqual(note.data, "Odwa")
         self.assertEqual(note.date, 2020/5/21)
         self.assertEqual(note.user_id, 1)
+
